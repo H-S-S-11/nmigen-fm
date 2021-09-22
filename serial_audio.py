3275e004ce4759f55dfb1180d555b257ba27a408
H-S-S-11 <harry@snell.org.uk>
2021-09-22 14:51:58 +0100

    basic music player

---
+++ serial_audio.py
@@ -0,0 +1,32 @@
+from scipy.signal.signaltools import sosfilt
+import serial
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+from scipy import signal
+import time
+import io
+
+
+ser = serial.Serial('COM8', 441000)
+
+# try:
+#     while True:
+#         ser.write(b'z')
+#         print(ser.read())
+#         time.sleep(1/440)
+#         ser.write(b' ')
+#         time.sleep(1/440)
+# 
+# except KeyboardInterrupt:
+#     pass
+
+with open("bangarang-44k.wav", mode="rb") as audio:
+    try:
+        while True:
+            ser.write(audio.read())
+    except KeyboardInterrupt:
+        pass
+
+ser.close()
+            
